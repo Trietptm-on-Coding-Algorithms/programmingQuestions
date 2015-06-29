69a5e0e541ff284cb86ee1fdf9d5d5d257f232f6
Charles M. Chong <ncmd@users.noreply.github.com>
2015-06-29 15:42:13 -0700

    Update findDirListFiles.bash

--- findDirListFiles.bash
+++ findDirListFiles.bash
@@ -1,5 +1,6 @@
 # In the scripting language of your choice, write a quick script to count the number of files in a directory called "baz"
 #!/bin/bash
+# Created by Charles M. Chong
 a=$(expr 0)
 dir=$(find -maxdepth 1 -type d -name 'baz' | awk 'NR==1')
 for i in $( ls $dir )

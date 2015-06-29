1c52c44e1714820ed6ce43024bbf99fed59dbe10
ncmd <ncmd@users.noreply.github.com>
2015-06-28 23:27:23 -0700

    Update cracklePop.bash

--- cracklePop.bash
+++ cracklePop.bash
@@ -3,6 +3,7 @@
 # If the number is divisible by 3, print Crackle instead of the number. 
 # If it's divisible by 5, print Pop. If it's divisible by both 3 and 5, print CracklePop. 
 # You can use any language.
+# Created By Charles M. Chong
 
 rice=$(expr 1)
 crispies=$(expr 100)

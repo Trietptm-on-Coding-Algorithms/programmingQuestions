618b182e6923115aad681043c156a04af8f21601
ncmd <ncmd@users.noreply.github.com>
2015-06-28 23:18:57 -0700

    Update cracklePop.bash

--- cracklePop.bash
+++ cracklePop.bash
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Write a program that prints out the numbers 1 to 100 (inclusive). 
 # If the number is divisible by 3, print Crackle instead of the number. 
 # If it's divisible by 5, print Pop. If it's divisible by both 3 and 5, print CracklePop. 
@@ -8,21 +9,21 @@ crispies=$(expr 100)
 until [$rice -gt $crispies ]; do
 if (( $rice % 3 != 0 )) && (( $rice % 5 != 0 ))
 then
-echo $rice
-if (( $rice % 3 == 0 ))
-then
+  echo $rice
+  if (( $rice % 3 == 0 ))
+    then
+    if (( $rice % 5 == 0 ))
+      then
+      echo "CracklePop"
+      else
+      echo "Crackle"
+    fi
+  fi
 if (( $rice % 5 == 0 ))
-then
-echo "CracklePop"
-else
-echo "Crackle"
-fi
-fi
-if (( $rice % 5 == 0 ))
-then
-rice=$(expr $rice + 1)
-exho "Pop"
-else
-rice=$(expr $rice + 1)
+  then
+    rice=$(expr $rice + 1)
+    echo "Pop"
+  else
+    rice=$(expr $rice + 1)
 fi
 done

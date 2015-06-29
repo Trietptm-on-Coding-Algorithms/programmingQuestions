24f4f60e856e871b9a5acd6b9a4eb0fcf2ceb186
ncmd <ncmd@users.noreply.github.com>
2015-06-29 01:18:13 -0700

    Create findDirListFiles.bash

---
+++ findDirListFiles.bash
@@ -0,0 +1,9 @@
+# In the scripting language of your choice, write a quick script to count the number of files in a directory called "baz"
+#!/bin/bash
+a=$(expr 0)
+dir=$(find -maxdepth 1 -type d -name 'baz' | awk 'NR==1')
+for i in $( ls $dir )
+do 
+echo $a
+a=$(expr $a + 1)
+done
